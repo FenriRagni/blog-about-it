b4853125c75ab6f1505c899c33e328e6d1264063
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-11-06 19:43:43 -0500

    add skeleton for models

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,4 +1,4 @@
-const router = require('experess').Router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('all');
--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'I like turrles',
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+}
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
